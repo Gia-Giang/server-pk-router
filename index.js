9eb154a8b275949a177a0e70aaca678eb20f78bc
Gia-Giang <giang06112000@gmail.com>
2023-03-02 15:48:44 +0700

    fix

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.post("/github-webhook", (req, res) => {
   res.send("Github webhook successfully");
 });
 
+app.post("/github-webhook-api", (req, res) => {
+  res.send("Github webhook successfully");
+});
+
 app.listen(3000, () => {
   console.log("ss");
 });
