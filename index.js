f17e5042eb80d3a4a1db87fd045cf5d306c475f0
Gia-Giang <giang06112000@gmail.com>
2023-03-06 22:53:18 +0700

    test

--- index.js
+++ index.js
@@ -5,104 +5,20 @@ require("dotenv").config();
 let port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("hello worasdasdsadld");
+  res.send("hello world asasdasdsadsadsadsad");
 });
 
 app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
+  res.send("Github webhook successfully");
 });
 
 app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
+  res.send("Github webhook successdfully");
 });
 
 app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
+  res.send("Github webhook successdfully");
 });
-app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
-});
-
-app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
-});
-
-app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
-});
-
-app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
-});
-
-app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
-});
-
-app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
-});
-
-app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
-});
-
-app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
-});
-
-app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
-});
-
-app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
-});
-
-app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
-});
-
-app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
-});
-
-app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
-});
-
-app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
-});
-
-app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
-});
-
-app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
-});
-
-app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
-});
-
-app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
-});
-
-app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successasdasdfully");
-});
-
-app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfasdully");
-});
-
-app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successasdasdasdsadasdasdfully");
-});
-
 
 app.listen(port, () => {
   console.log("sssss", port);
