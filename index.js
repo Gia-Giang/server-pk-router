d04be1499b31b7e286a12c2fb4c955450ff8d3d3
Gia-Giang <giang06112000@gmail.com>
2023-03-06 22:07:06 +0700

    add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 let port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello world giang");
 });
 
 app.post("/github-webhook", (req, res) => {
