3b35f5ddf23203bd1627614ccf6bad00a8900878
Gia-Giang <giang06112000@gmail.com>
2023-03-06 22:02:40 +0700

    ss

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+const { EmbedBuilder, WebhookClient } = require('discord.js');
+
+const webhookClient = new WebhookClient({ id: "1077981490790547466", token: "zj-OvyjXxcUiUl4r7gF670OAxcUj9GNR5z7RX9s7gAO6sQ5Iur8MfH4jvkh59K6WRqHU" });
+
+const embed = new EmbedBuilder()
+	.setTitle('Some Title')
+	.setColor(0x00FFFF);
+
+webhookClient.send({
+	content: 'Webhook test',
+	username: 'some-username',
+	avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+	embeds: [embed],
+});
+
 require("dotenv").config();
 let port = process.env.PORT;
 
