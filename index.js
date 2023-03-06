8b6ecd7946bca96c08968ea0f754047e9ec9feff
Gia-Giang <giang06112000@gmail.com>
2023-03-06 22:40:26 +0700

    ss

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ require("dotenv").config();
 let port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello worasdasdsadld");
 });
 
 app.post("/github-webhook", (req, res) => {
-  res.send("Github webhook successfully");
+  res.send("Github webhook successasdasdfully");
 });
 
 app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook successdfully");
+  res.send("Github webhook successdfasdully");
 });
 
 app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook successdfully");
+  res.send("Github webhook successasdasdasdsadasdasdfully");
 });
 
 app.listen(port, () => {
