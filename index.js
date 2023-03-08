029e055f61d25199c25b3fe51b87fc5e46983312
Gia-Giang <giang06112000@gmail.com>
2023-03-08 20:42:25 +0700

    ss

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ app.post("/github-webhook", (req, res) => {
 });
 
 app.post("/github-webhook-apssi", (req, res) => {
-  res.send("Github webhook ");
+  res.send("Github webhook asdsadasd");
 });
 
 app.post("/github-webhook-assssd", (req, res) => {
-  res.send("Github webhook sss");
+  res.send("Github webhook sssdasdsas");
 });
 
 app.listen(port, () => {
